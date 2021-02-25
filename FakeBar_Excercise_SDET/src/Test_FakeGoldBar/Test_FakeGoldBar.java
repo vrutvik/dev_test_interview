f2d96256c872b9ba81f5ee6098f24eb7b5e0f281
vrutvik <79629871+vrutvik@users.noreply.github.com>
2021-02-25 14:56:30 -0600

    Update Test_FakeGoldBar.java
    reset button click updated

--- FakeBar_Excercise_SDET/src/Test_FakeGoldBar/Test_FakeGoldBar.java
+++ FakeBar_Excercise_SDET/src/Test_FakeGoldBar/Test_FakeGoldBar.java
@@ -13,69 +13,65 @@ public class Test_FakeGoldBar {
 	public static int chunk = size/3;
 	public static int i = 0;
 	public static String result = "Yay! You find it!";
+	public static int x = chunk;
 	public static ChromeDriver driver;
 	static void input_values() {
-		for(int k=i;k<chunk;k++) {
+		int k;
+		int j;
+		for(k=i;k<x;k++) {
 			String S = k+"";
 		driver.findElementByXPath("//div[@class='board-row']//input[@id='left_"+S+"']").clear();
 		driver.findElementByXPath("//div[@class='board-row']//input[@id='left_"+S+"']").sendKeys("value",""+k);
 		}
-		for(int j = i ; j<chunk;j++) {
+		for(j = i ; j<x;j++) {
 			String S2 = j+3+"";
 			driver.findElementByXPath("//div[@class='board-row']//input[@id='right_"+j+"']").clear();
 			driver.findElementByXPath("//div[@class='board-row']//input[@id='right_"+j+"']").sendKeys("value",""+S2+"");
 		}
+		driver.findElementByXPath("//*[@id='weigh']").click();
+		String op = driver.findElementByXPath("//div[@class='result']//button[@id='reset']").getText();
+		if(op.equals('=')) {
+			driver.findElementByXPath("//button[.='Reset']").click();
+			System.out.println("Reset");
+			x=+3;
+			i=+3;
+			if(x>size) {
+				System.exit(-1);
+			}
+			input_values();
+		}
+		else {
+			check_values();
+		
+		}
+		
 	}
 	static void check_values() {
 		int q = i;
-		while(q<=chunk) {
+		for(q=i;q<chunk;q++) {
 			driver.findElementByXPath("//*[@id='coin_"+q+"']").click();
+			System.out.println("click on coin"+q);
 			Alert al = driver.switchTo().alert();
-			//driver.switchTo().alert().sendKeys("Text");
 			String Al_m = driver.switchTo().alert().getText();
-			al.accept();
 			if(Al_m==result) {
 				System.out.println("We got the results."+Al_m);
-				break;
+				System.exit(1);
 			}
-			else{
-				//al.accept();
-				check_values();
+			al.accept();
+			System.out.println("click on accept & checking values");
+			check_values();
 				//System.out.println("No Luck!");
-			}
-			break;	
 		}
+		input_values();	
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		 driver= new ChromeDriver();
 		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
 		driver.get("http://ec2-54-208-152-154.compute-1.amazonaws.com/");
+		driver.manage().window().maximize();
 		driver.getTitle();
-		
-//		for(int k=i;k<chunk;k++) {
-//			String S = k+"";
-//		driver.findElementByXPath("//div[@class='board-row']//input[@id='left_"+S+"']").clear();
-//		driver.findElementByXPath("//div[@class='board-row']//input[@id='left_"+S+"']").sendKeys("value",""+k);
-//		}
-//		for(int j = i ; j<chunk;j++) {
-//			String S2 = j+3+"";
-//			driver.findElementByXPath("//div[@class='board-row']//input[@id='right_"+j+"']").clear();
-//			driver.findElementByXPath("//div[@class='board-row']//input[@id='right_"+j+"']").sendKeys("value",""+S2+"");
-//		}
 		input_values();
-		driver.findElementByXPath("//*[@id='weigh']").click();
-		String op = driver.findElementByXPath("//div[@class='result']//button[@id='reset']").getText();
-		if(op.equals('=')) {
-			i++;
-			input_values();
-		}
-		else {
-			i++;
-			check_values();
-		
-	}
-	
 		//driver.findElementByXPath("//div[@class='board-row']//input[@id='right_"+S2+"']").sendKeys("value",""+k);
 		}
 }
